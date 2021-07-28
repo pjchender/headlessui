42d2d935f1d199c7633b665b1427d6e8c0aacad1
aaronchen <aaron.chen@onedegree.hk>
2021-07-29 00:25:25 +0800

    docs(utils): add note on 'match'

--- packages/@headlessui-react/src/utils/match.ts
+++ packages/@headlessui-react/src/utils/match.ts
@@ -1,3 +1,22 @@
+/**
+ * 說明與範例
+ * lookup 是物件，value 是想要從 lookup 中找的 key
+ * 從 lookup 中找出 key 為 value 的
+ * 物件值是函式的話，會執行這個函式，並以 match 後多餘的參數當作參數傳入
+ * 否則回傳 lookup 物件對應的 value
+ *
+ * enum GENDER {
+ *   MALE,
+ *   FEMALE,
+ * }
+ * const lookup = {
+ *   [GENDER.MALE]: 'man',
+ *   [GENDER.FEMALE]: 'woman',
+ * };
+ *
+ * const result = match(GENDER.MALE, lookup) // "man"
+ **/
+
 export function match<TValue extends string | number = string, TReturnValue = unknown>(
   value: TValue,
   lookup: Record<TValue, TReturnValue | ((...args: any[]) => TReturnValue)>,
